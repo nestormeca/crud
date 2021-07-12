47b0bb02d56bcf969e44f692b08e34167ba8d6ca
nestormeca <nestor.m3@gmail.com>
2021-07-11 21:35:45 -0400

    agregue useNew en mongoose

--- src/app.js
+++ src/app.js
@@ -1,36 +1,35 @@
-
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // connecting to db
 
-mongoose.connect('mongodb://localhost/dbbodega')
-    .then(db => console.log('DB conected'))
-    .catch(err => console.log(err));
-
+mongoose
+  .connect("mongodb://localhost/dbbodega", { useNewUrlParser: true })
+  .then((db) => console.log("DB conected"))
+  .catch((err) => console.log(err));
 
 //importing routes
-const indexRoutes = require('./routes/index');
+const indexRoutes = require("./routes/index");
 
 //settings
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 //middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use('/', indexRoutes);
+app.use("/", indexRoutes);
 
 //starting the server
 
-app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
 });
